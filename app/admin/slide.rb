36a7e62cc76ba28aed5ab2890eb1d7a9a87ac46c
Justin <gdjyluxiaoyong@gmail.com>
2016-11-05 11:45:42 +0800

    添加轮播图显示效果

--- app/admin/slide.rb
+++ app/admin/slide.rb
@@ -2,6 +2,21 @@ ActiveAdmin.register Slide do
 
 permit_params :desc, :image, :is_show, :weight, :url
 
+index do
+  selectable_column
+  id_column
+
+  column "轮播图" do |slide|
+    image_tag(slide.image_url, size: "72x45", :alt => "slide image")
+  end
+  column :desc
+  column :is_show
+  column :weight
+  column :url
+  column :updated_at
+  actions
+end
+
 form(:html => { :multipart => true }) do |f|
   f.inputs "Slide" do
     f.input :desc
