8352031b6b9924eb48b9cf71932588b2079f6a93
Justin <gdjyluxiaoyong@gmail.com>
2016-11-05 20:41:08 +0800

    修正ckeditor asset precompile

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -55,8 +55,6 @@ Ckeditor.setup do |config|
   # Used when CKEditor CDN enabled
   # By default: "/assets/ckeditor/config.js"
   # config.js_config_url = "/assets/ckeditor/config.js"
-  Ckeditor.setup do |config|
-    config.assets_languages = ['en']
-    config.assets_plugins = ['image', 'smiley']
-  end
+  config.assets_languages = ['en']
+  config.assets_plugins = ['image', 'smiley']
 end
