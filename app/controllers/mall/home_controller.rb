0a3519998364ffe84d6402193c88c69bca4d7f55
suifeng <ganweiliang886@gmail.com>
2016-11-10 22:07:58 +0800

    修复代码(微信首页)

--- app/controllers/mall/home_controller.rb
+++ app/controllers/mall/home_controller.rb
@@ -1,6 +1,6 @@
 class Mall::HomeController < Mall::BaseController
   def index
-    @slides = Slide.where(is_show: true, tp: 2).order(weight: :asc).limit(3)
+    @slides = Slide.top(2)
     @vip_types = VipType.all
     @setmeal = Setmeal.all
   end
--- app/controllers/wechat/home_controller.rb
+++ app/controllers/wechat/home_controller.rb
@@ -1,6 +1,6 @@
 class Wechat::HomeController < Wechat::BaseController
   def index
-    @slides = Slide.where(is_show: true, tp: 1).order(weight: :asc).limit(3)
+    @slides = Slide.top(1)
     @vip_types = VipType.all
     @setmeal = Setmeal.all
   end
