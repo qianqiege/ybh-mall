a4e2cc9a7e5f5333d94b3243f9229613e4eaa28b
xiao <skjmy@outlook.com>
2016-11-14 11:56:41 +0800

    修改Product中价格的 数据类型 （decimal）

---
+++ db/migrate/20161114034620_change_original_product_price_from_product.rb
@@ -0,0 +1,6 @@
+class ChangeOriginalProductPriceFromProduct < ActiveRecord::Migration[5.0]
+  def change
+    change_column :products, :original_product_price, :decimal,:precision => 50,:scale=>2
+    change_column :products, :now_product_price, :decimal,:precision => 50,:scale=>2
+  end
+end
