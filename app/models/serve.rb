c5a66b937d36141e5fd46bcc3381a388a051e54c
xiao <skjmy@outlook.com>
2016-11-15 15:16:52 +0800

    绑定 服务关系

--- app/models/serve.rb
+++ app/models/serve.rb
@@ -3,4 +3,6 @@ class Serve < ApplicationRecord
   has_many :membership_cards
   has_many :setmeals, through: :set_meal_serve_relations
   has_many :set_meal_serve_relations
+  has_many :reservation_records
+  has_many :service_staffs
 end
