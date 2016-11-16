1982cc533bda8ed4c2393a3c0d9de0c45a2cca10
xiao <skjmy@outlook.com>
2016-11-16 13:36:37 +0800

    建立 服务人员 工作站 等级 脊筑师之间的关系

--- app/models/spine_build.rb
+++ app/models/spine_build.rb
@@ -1,2 +1,5 @@
 class SpineBuild < ApplicationRecord
+  belongs_to :workstation
+  belongs_to :rank
+  belongs_to :serve
 end
--- db/migrate/20161116040133_create_spine_builds.rb
+++ db/migrate/20161116040133_create_spine_builds.rb
@@ -3,6 +3,9 @@ class CreateSpineBuilds < ActiveRecord::Migration[5.0]
     create_table :spine_builds do |t|
       t.string :name
 
+      t.integer :workstation_id
+      t.integer :rank_id
+      t.integer :serve_id
       t.timestamps
     end
   end
