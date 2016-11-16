c56c86f8bf51d8b9594a761d8895c34f3188da13
xiao <skjmy@outlook.com>
2016-11-16 11:48:31 +0800

    修改 服务人员（筑脊师、健康管理师等） 表中字段

---
+++ db/migrate/20161116034646_remove_name_from_service_staff.rb
@@ -0,0 +1,8 @@
+class RemoveNameFromServiceStaff < ActiveRecord::Migration[5.0]
+  def change
+    remove_column :service_staffs, :name, :string
+    remove_column :service_staffs, :grade, :string
+    remove_column :service_staffs, :city, :string
+    remove_column :service_staffs, :serve_number, :integer
+  end
+end
