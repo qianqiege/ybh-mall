9769989221389655099d62e8435bcd53cf39eb25
xiao <skjmy@outlook.com>
2016-11-16 11:52:24 +0800

    清除 serve_id From 服务人员表

--- db/migrate/20161116034646_remove_name_from_service_staff.rb
+++ db/migrate/20161116034646_remove_name_from_service_staff.rb
@@ -4,5 +4,6 @@ class RemoveNameFromServiceStaff < ActiveRecord::Migration[5.0]
     remove_column :service_staffs, :grade, :string
     remove_column :service_staffs, :city, :string
     remove_column :service_staffs, :serve_number, :integer
+    remove_column :service_staffs, :serve_id, :integer
   end
 end
