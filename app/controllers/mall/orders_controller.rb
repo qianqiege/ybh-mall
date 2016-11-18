87867568037b1fc0bac45ba6e8546a6ef9cb7dd1
hfpp2012 <hfpp2012@aliyun>
2016-11-18 21:51:52 +0800

    change cart show

--- app/controllers/mall/orders_controller.rb
+++ app/controllers/mall/orders_controller.rb
@@ -1,9 +1,12 @@
 class Mall::OrdersController < Mall::BaseController
   include BindPhone
-  before_action :check_bind_phone, only: [:create]
+  before_action :check_bind_phone, only: [:confirm]
   include HasAddress
-  before_action :check_has_address, only: [:create]
+  before_action :check_has_address, only: [:confirm]
 
   def create
+  end
+
+  def confirm
   end
 end
