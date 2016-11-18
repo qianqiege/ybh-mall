8efedd38c462e81b218775ee40577b9b67093f81
Justin <gdjyluxiaoyong@gmail.com>
2016-11-18 21:31:55 +0800

    update migration

--- db/migrate/20161116090305_change_shop_count_to_integer_from_products.rb
+++ db/migrate/20161116090305_change_shop_count_to_integer_from_products.rb
@@ -1,5 +1,6 @@
 class ChangeShopCountToIntegerFromProducts < ActiveRecord::Migration[5.0]
   def change
-    change_column :products, :shop_count, :integer
+    remove_column :products, :shop_count
+    add_column :products, :shop_count, :integer, default: 0
   end
 end
