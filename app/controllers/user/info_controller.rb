199b67f0faf1e31bb5b74726063df44b72c9c124
xiao <skjmy@outlook.com>
2016-12-07 15:21:41 +0800

    添加 健康档案 显示页面

--- app/controllers/user/info_controller.rb
+++ app/controllers/user/info_controller.rb
@@ -1,6 +1,15 @@
 class User::InfoController < Wechat::BaseController
-  def show
-    @vip_record = VipRecord.all
-    @wechat_user = WechatUser.all
+  def home
+    @vip_record = VipRecord.find(params[:format])
+    @wechat_user = WechatUser.find(current_user)
+  end
+
+  def helath_record
+    mall = Sdk::Mall.new
+    @record = mall.record(100000000000000013)
+  end
+
+  def wechat_info
+    @wechat_info = WechatUser.find(current_user)
   end
 end
