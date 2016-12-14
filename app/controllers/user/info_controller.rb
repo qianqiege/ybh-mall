311881684b8b92ce7c5afe18eb04be2cb979833d
xiao <skjmy@outlook.com>
2016-12-14 14:56:53 +0800

    添加微信信息展示 更改渲染数据位置

--- app/controllers/user/info_controller.rb
+++ app/controllers/user/info_controller.rb
@@ -1,15 +1,10 @@
 class User::InfoController < Wechat::BaseController
   def home
-    if !@vip_record.nil?
-      @vip_record = VipRecord.find(params[:format])
-    end
-    if !@wechat_user.nil?
-      @wechat_user = WechatUser.find(current_user)
-    end
+    @wechat_user = WechatUser.find(current_user)
   end
 
   def helath_record
-    @idcard = VipRecord.find(params[:format])
+    @idcard = User.find(current_user.user_id)
     mall = Sdk::Mall.new
     @record = mall.record(@idcard.identity_card)
   end
