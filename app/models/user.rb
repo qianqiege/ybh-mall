d230f5d4281793bfe61cf2d2b824bd1461e9eb0d
xiao <skjmy@outlook.com>
2016-12-15 10:47:40 +0800

    添加数据验证

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   # has_many :subscribe 预约
   has_one :order
   has_many :examine_records
+
+  def name
+    telphone
+  end
+
+  validates :telphone, presence: true, length: {is: 11}
+  validates :password, presence: true, length: { in: 6..20 }
+  validates :identity_card, presence: true, length: { is: 18 }
 end
