3e9a79908fff0c232f3f0e8cdaaea66db9411ebe
xiao <skjmy@outlook.com>
2017-01-04 11:31:36 +0800

    将预约手机号 更换为身份证号码 并且加入非空验证

--- app/models/reservation_record.rb
+++ app/models/reservation_record.rb
@@ -2,5 +2,7 @@ class ReservationRecord < ApplicationRecord
   attr_accessor :rank_id, :work_id
 
   belongs_to :spine_build
+  validates :spine_build, :name, presence: true
+  validates :identity_card, length: { is: 18 }
 end
 # class="weui-select"
