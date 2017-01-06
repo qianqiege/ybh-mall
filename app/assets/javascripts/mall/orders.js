740ca0f321e21a6b44b2c60699b40176e1600832
suifeng <ganweiliang886@gmail.com>
2017-01-07 01:50:01 +0800

    cancel order

--- app/assets/javascripts/mall/orders.js
+++ app/assets/javascripts/mall/orders.js
@@ -16,4 +16,24 @@ $(function () {
   $("#return_request_btn").on('click', function() {
     $("#new_return_request").submit();
   })
+
+  $(".btn-cancel").on('click', function() {
+    $.confirm('确定要取消此订单吗？', function(e) {
+      var id = $(this).data('id');
+      if (e) {
+        $.ajax({
+          type: 'POST',
+          url: '/mall/orders/' + id + '/make_cancel',
+          data: {},
+          dataType: 'json',
+          success: function(data) {
+            window.location.reload();
+          },
+          error: function() {
+            $.tips('出错了');
+          }
+        })
+      }
+    }.bind(this));
+  })
 });
