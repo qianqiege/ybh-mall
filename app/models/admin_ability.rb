e0c9a4b47d8aa7c101352d93f2e3c5f88e610043
xiao <skjmy@outlook.com>
2017-01-09 17:07:17 +0800

    添加筑脊部 权限

--- app/models/admin_ability.rb
+++ app/models/admin_ability.rb
@@ -7,6 +7,10 @@ class AdminAbility
     elsif user.role_name == 'db'
       can :manage, Order
       can :manage, ReturnRequest
+    elsif user.role_name == 'spinebuild'
+      can :manage, Rank
+      can :manage, SpineBuild
+      can :manage, Workstation
     end
     can :read, ActiveAdmin::Page, name: "Dashboard"
   end
