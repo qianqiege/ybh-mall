1b0a6ba9ff822a4c51f73e9cca77e41d4cd7781b
xiao <skjmy@outlook.com>
2017-01-09 10:30:44 +0800

    更改 处方API

--- app/controllers/h_programs_controller.rb
+++ app/controllers/h_programs_controller.rb
@@ -1,14 +1,12 @@
-class ProductProgramController < ApplicationController
+class HProgramsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
-  def products
+  def create
     @health_program = HealthProgram.new(identity_card: params[:identity_card],
-                                        name: params[:name],
-                                        number: params[:number],
-                                        only_number: params[:only_number],
                                         time: params[:time],
                                         coding: params[:coding])
     if @health_program.save
+      @product_program = ProductProgram.new
       render json: @health_program
     else
       render_error("错误")
--- app/models/health_program.rb
+++ app/models/health_program.rb
@@ -1,5 +1,4 @@
 class HealthProgram < ApplicationRecord
-  validates :name,:number,:only_number,:coding, presence: true
-  validates :identity_card , presence: true ,length: { is: 18 }
+  # validates :identity_card , presence: true ,length: { is: 18 }
   has_many :product_programs
 end
