c7e51e98248aff4c969f79586447fbaf37b86083
xiao <skjmy@outlook.com>
2017-01-11 16:52:08 +0800

    修改权限 基础数据

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 if AdminUser.first.blank?
-  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+  AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password',role_name: 'admin')
 end
 
 if HealthExamine.first.blank?
