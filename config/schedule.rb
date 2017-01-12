d1c7a36b9124fc4e3fbfbd43f32e33ca3903b9c8
Justin <gdjyluxiaoyong@gmail.com>
2017-01-12 12:27:53 +0800

    删除定时器测试

--- config/schedule.rb
+++ config/schedule.rb
@@ -9,10 +9,6 @@ log_path = ::File.expand_path('../../log/cron_log.log', __FILE__)
 FileUtils.touch log_path unless File.exist? log_path
 set :output, log_path
 
-# every 1.day, :at => '4:30 am' do
-#   rake 'users:update_coin'
-# end
-
-every 1.minutes do
+every 1.day, :at => '4:30 am' do
   rake 'users:update_coin'
 end
