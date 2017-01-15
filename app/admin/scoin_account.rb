8546baf4980c9adf18e315aec06ddd090d3c9b10
Justin <gdjyluxiaoyong@gmail.com>
2017-01-15 12:41:24 +0800

    新建账户订单绑定根据用户过滤订单

--- app/admin/scoin_account.rb
+++ app/admin/scoin_account.rb
@@ -5,10 +5,6 @@ ActiveAdmin.register ScoinAccount do
   form do |f|
     f.inputs "S币账户" do
       f.input :user
-      # f.input :order
-      # f.input :order_id, as: :nested_select,
-      #                    level_1: { attribute: :user_id, fields: [:identity_card], display_name: :identity_card, minimum_input_length: 0 },
-      #                    level_2: { attribute: :order_id, fields: [:number], display_name: :number, minimum_input_length: 0 }
       f.input :account
       f.input :password
     end
--- app/admin/scoin_account_order_relation.rb
+++ app/admin/scoin_account_order_relation.rb
@@ -2,10 +2,22 @@ ActiveAdmin.register ScoinAccountOrderRelation do
   menu parent: I18n.t("active_admin.menu.coin_manage")
   permit_params :order_id, :scoin_account_id
 
+  collection_action :user_orders, method: :get do
+    user_id = ScoinAccount.find_by(params[:q][:scoin_account_id_eq]).try(:user_id)
+    query = params[:q]
+    query.delete(:scoin_account_id_eq)
+    query[:user_id_eq] = user_id
+    render json: Order.search(query).result
+  end
+
   form do |f|
     f.inputs "S币账户" do
-      f.input :order
-      f.input :scoin_account
+      # f.input :order
+      # f.input :scoin_account
+      f.input :order_id, as: :nested_select,
+                         level_1: { attribute: :scoin_account_id, fields: [:account], display_name: :account, minimum_input_length: 0 },
+                         level_2: { attribute: :order_id, fields: [:number], display_name: :number, minimum_input_length: 0, url: "/admin/scoin_account_order_relations/user_orders" }
+
     end
     f.actions
   end
