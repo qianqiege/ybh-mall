4d594d7046606b3c1127baefbc9bef3d674a2b2e
Justin <gdjyluxiaoyong@gmail.com>
2017-01-16 20:55:23 +0800

    改用手机号码登陆

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :authentication_keys => [:telphone]
   has_one :member_record
   has_one :wechat_user
   has_many :orders
   has_many :examine_records
 
   validates :telphone, uniqueness: true, presence: true, length: {is: 11}
-  validates :password, presence: true, length: { in: 6..20 }
   validates :identity_card, uniqueness: true, presence: true, length: { is: 18 }
-
 
   def name
     identity_card
