6937647cea722f8dccacf8c9cb88bb562c828a96
xiao <skjmy@outlook.com>
2017-02-13 16:12:31 +0800

    添加副本编辑器

--- app/admin/product.rb
+++ app/admin/product.rb
@@ -40,7 +40,7 @@ ActiveAdmin.register Product do
       f.input :shop_count
       f.input :is_show
       f.input :production
-      f.input :product_sort
+      f.input :product_sort,:as => :ckeditor
       f.input :only_number
       f.input :packaging
       f.input :image, as: :file
