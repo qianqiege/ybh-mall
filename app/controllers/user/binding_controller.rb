04625f43b737157712f51b78808804df521e7826
xiao <skjmy@outlook.com>
2017-02-15 09:50:46 +0800

    修改无人邀请报错，修改随机数首数字为0忽略

--- app/controllers/user/binding_controller.rb
+++ app/controllers/user/binding_controller.rb
@@ -4,19 +4,31 @@ class User::BindingController < Wechat::BaseController
   end
 
   def bind_phone
+
     if sms_code_validate(params[:code], params[:mobile])
       current_user.update_mobile(params[:mobile])
       # 绑定有折扣的会员vip
-      @invitation_card = rand(9999999999)
+      @invitation_card = rand(1111111111..9999999999)
       if !params[:invitation_user].nil?
         @invitation_user = User.find_by(invitation_card: params[:invitation_user])
-        @user = User.new(name: params[:name],telphone: params[:mobile],password: params[:password],identity_card: params[:identity_card],invitation_card: @invitation_card,invitation_user: @invitation_user.name)
-        @current_user = current_user.id
-        if @user.save && @wechat = WechatUser.where('id LIKE ?',"%#{@current_user}%").update_all(user_id: @user.id)
-          redirect_to user_root_path
+        if !@invitation_user.nil?
+          @user = User.new(name: params[:name],telphone: params[:mobile],password: params[:password],identity_card: params[:identity_card],invitation_card: @invitation_card,invitation_user: @invitation_user.name)
+          @current_user = current_user.id
+          if @user.save && @wechat = WechatUser.where('id LIKE ?',"%#{@current_user}%").update_all(user_id: @user.id)
+            redirect_to user_root_path
+          else
+            flash[:notice] = '绑定失败'
+            redirect_back fallback_location: user_binding_path
+          end
         else
-          flash[:notice] = '绑定失败'
-          redirect_back fallback_location: user_binding_path
+          @user = User.new(name: params[:name],telphone: params[:mobile],password: params[:password],identity_card: params[:identity_card],invitation_card: @invitation_card)
+          @current_user = current_user.id
+          if @user.save && @wechat = WechatUser.where('id LIKE ?',"%#{@current_user}%").update_all(user_id: @user.id)
+            redirect_to user_root_path
+          else
+            flash[:notice] = '绑定失败'
+            redirect_back fallback_location: user_binding_path
+          end
         end
       end
     else
