8c15a9d8fcd06e21ffac7f1c5dd8fd737a03d6b6
xiao <skjmy@outlook.com>
2017-02-21 17:06:31 +0800

    暂时将商城 绑定页面修改为 其他绑定页面

--- app/controllers/concerns/bind_phone.rb
+++ app/controllers/concerns/bind_phone.rb
@@ -7,9 +7,11 @@ module BindPhone
     unless bind_phone?
       store_location
       if request.xhr?
-        render json: { location: mall_authenticate_phone_path }
+        # render json: { location: mall_authenticate_phone_path }
+        render json: { location: user_binding_path }
       else
-        redirect_to mall_authenticate_phone_path
+        # redirect_to mall_authenticate_phone_path
+        redirect_to user_binding_path
       end
     end
   end
