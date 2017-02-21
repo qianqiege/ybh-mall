a93c2b0330f0b2dba942691e548e03411ac52a68
Justin <gdjyluxiaoyong@gmail.com>
2017-02-21 23:01:40 +0800

    log自动按日期切分

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,10 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  # auto rotate log in every day
+  # http://ruby-doc.org/stdlib-2.2.0/libdoc/logger/rdoc/Logger.html#method-c-new
+  config.logger = Logger.new("#{Rails.root}/log/#{Rails.env}.log", 'daily')
+
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
