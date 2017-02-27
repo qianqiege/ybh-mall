2ef5b9e6454c39df5bae312d9394e569d05d8094
xiao <skjmy@outlook.com>
2017-02-27 17:37:01 +0800

    添加 机构身份证API的路由

--- config/routes.rb
+++ config/routes.rb
@@ -120,6 +120,10 @@ Rails.application.routes.draw do
     post :create, on: :collection
   end
 
+  resources :health_data do
+    post :create, on: :collection
+  end
+
   root "wechat/home#index"
   post 'putmessage' => 'devices#putmessage'
 
