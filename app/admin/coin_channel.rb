290dff208f7b3848387a7b149837c5f69aea7b27
skjmy <skjmy@outlookc.om>
2017-03-15 10:10:53 +0800

    修改admin编辑写入权限

--- app/admin/coin_channel.rb
+++ app/admin/coin_channel.rb
@@ -1,17 +1,4 @@
 ActiveAdmin.register CoinChannel do
-
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
-
-
+  menu parent: I18n.t("active_admin.menu.coin_manage")
+  permit_params :name
 end
