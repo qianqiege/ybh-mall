a9493273618bf897b9a6f2c5bf2913ba03daa2ad
Justin <gdjyluxiaoyong@gmail.com>
2017-03-19 21:20:42 +0800

    修改admin 易币

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -9,7 +9,7 @@ ActiveAdmin.register User do
                 :invitation_user,
                 :organization_id,
                 :invitation_id,
-                :coin
+                :y_coin
 
   index do
     selectable_column
@@ -21,7 +21,7 @@ ActiveAdmin.register User do
     column :invitation_card
     column :invitation_id
     column :organization
-    column :coin
+    column :y_coin
     column :type
     actions defaults: true
   end
@@ -51,7 +51,7 @@ ActiveAdmin.register User do
       row :invitation_card
       row :invitation_id
       row :organization
-      row :coin
+      row :y_coin
     end
   end
 
