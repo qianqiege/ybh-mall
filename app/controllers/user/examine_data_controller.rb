b2d5e2f0c6a37e4e6c1c72507a2473d69898c4fd
skjmy <skjmy@outlookc.om>
2017-03-30 10:43:53 +0800

    添加折线图

--- app/controllers/user/examine_data_controller.rb
+++ app/controllers/user/examine_data_controller.rb
@@ -6,36 +6,70 @@ class User::ExamineDataController < Wechat::BaseController
   def show_blood_fat
     if !current_user.user_id.nil?
       @show = BloodFat.where(user_id: current_user.user_id)
+      @options = {
+        colors: ["#ff0000", "#add9c0"],
+        xAxis: {
+          type: 'datetime',
+          labels: {
+            format: '{value:%m-%d}',
+            align: 'right'
+          }
+        }
+      }
     end
   end
 
   def show_temperature
     if !current_user.user_id.nil?
       @show = Temperature.where(user_id: current_user.user_id)
+      @options = {
+        colors: ["#ff0000", "#add9c0"],
+        xAxis: {
+          type: 'datetime',
+          labels: {
+            format: '{value:%m-%d}',
+            align: 'right'
+          }
+        }
+      }
     end
   end
 
   def show_weight
     if !current_user.user_id.nil?
       @show = Weight.where(user_id: current_user.user_id)
+      @options = {
+        colors: ["#ff0000", "#add9c0"],
+        xAxis: {
+          type: 'datetime',
+          labels: {
+            format: '{value:%m-%d}',
+            align: 'right'
+          }
+        }
+      }
     end
   end
 
   def show_glucose
     if !current_user.user_id.nil?
       @show = BloodGlucose.where(user_id: current_user.user_id)
+      @options = {
+        colors: ["#ff0000", "#add9c0"],
+        xAxis: {
+          type: 'datetime',
+          labels: {
+            format: '{value:%m-%d}',
+            align: 'right'
+          }
+        }
+      }
     end
   end
 
   def show_heart
     if !current_user.user_id.nil?
       @show = HeartRate.where(user_id: current_user.user_id)
-    end
-  end
-
-  def show_pressure
-    if !current_user.user_id.nil?
-      @show = BloodPressure.where(user_id: current_user.user_id)
       @options = {
         colors: ["#ff0000", "#add9c0"],
         xAxis: {
@@ -49,8 +83,36 @@ class User::ExamineDataController < Wechat::BaseController
     end
   end
 
-  def show_unine
+  def show_pressure
+    if !current_user.user_id.nil?
+      @show = BloodPressure.where(user_id: current_user.user_id).order(updated_at: :desc)
+      @options = {
+        colors: ["#ff0000", "#add9c0"],
+        xAxis: {
+          type: 'datetime',
+          labels: {
+            format: '{value:%m-%d}',
+            align: 'right'
+          }
+        }
+      }
+    end
+  end
 
+  def show_unine
+    if !current_user.user_id.nil?
+      @show = Unine.where(user_id: current_user.user_id).order(updated_at: :desc)
+      @options = {
+        colors: ["#ff0000", "#add9c0"],
+        xAxis: {
+          type: 'datetime',
+          labels: {
+            format: '{value:%m-%d}',
+            align: 'right'
+          }
+        }
+      }
+    end
   end
 
 end
