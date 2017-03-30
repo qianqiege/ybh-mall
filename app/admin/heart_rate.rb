00b75d6e0d9c4bd5a0c94bb42c735532eccb9c04
skjmy <skjmy@outlookc.om>
2017-03-30 11:07:01 +0800

    让user_id展示在Admin中

--- app/admin/heart_rate.rb
+++ app/admin/heart_rate.rb
@@ -1,4 +1,15 @@
 ActiveAdmin.register HeartRate do
   menu parent: I18n.t("active_admin.menu.examine_manage")
   permit_params :value,:user_id,:examine_record_id,:phone,:state
+
+  index do
+    selectable_column
+    id_column
+
+    column :user
+    column :value
+    column :phone
+    column :state
+    actions
+  end
 end
