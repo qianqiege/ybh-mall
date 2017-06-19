958b5be06fda54495b9f565dd18484525dc6802a
skjmy <skjmy@outlookc.om>
2017-06-19 09:29:35 +0800

    定时器 9:40am

--- config/schedule.rb
+++ config/schedule.rb
@@ -13,6 +13,6 @@ every 1.day, :at => '4:30 am' do
   rake 'users:update_coin'
 end
 
-every 1.day, :at => '10:00 am' do
+every 1.day, :at => '9:40 am' do
   rake 'presented_record:update_locking_available'
 end
