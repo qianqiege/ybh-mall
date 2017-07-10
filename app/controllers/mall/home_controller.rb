fe2c151bd338fd9381bc87f1cfbb0cb8a8643876
skjmy <skjmy@outlookc.om>
2017-07-10 16:45:24 +0800

    修改产品控制器

--- app/controllers/mall/home_controller.rb
+++ app/controllers/mall/home_controller.rb
@@ -2,12 +2,12 @@ class Mall::HomeController < Mall::BaseController
   def index
     @slides = Slide.top(2)
     @q = Product.ransack(params[:q])
-    @products = @q.result(distinct: true).where(is_show: true).order("priority DESC, id DESC")
+    @products = @q.result(distinct: true).where(display: true).order("priority DESC, id DESC")
   end
 
   def classify
   	@q = Product.ransack(params[:q])
-  	@products = @q.result(distinct: true).where(is_show: true).order("priority DESC, id DESC")
+  	@products = @q.result(distinct: true).where(display: true).order("priority DESC, id DESC")
     @product1 = @products.where(sort: 1)
     @product2 = @products.where(sort: 2)
     @product3 = @products.where(sort: 3)
