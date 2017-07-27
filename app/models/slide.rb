eee104b4723d6d577a9eb5a301482f3716ca5e1b
skjmy <skjmy@outlookc.om>
2017-07-27 14:39:07 +0800

    添加滚动图片标识

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -5,6 +5,7 @@ class Slide < ApplicationRecord
                 '3'=>'服务首页图片' ,
                 '4'=>'御邦会会员卡图片',
                 '6'=>'方案首页轮播图',
-                '7'=>'订阅服务轮播图'}.freeze
+                '7'=>'订阅服务轮播图',
+                '8'=>'商盟首页轮播图片'}.freeze
   scope :top , ->(type) { where(is_show: true, tp: type).order(weight: :asc).limit(9) }
 end
