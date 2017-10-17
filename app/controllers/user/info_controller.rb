172c3edcbb05be5bbd9bf31b07ef9c96aaef337a
diyu123 <694310292@qq.com>
2017-10-17 12:29:33 +0800

    编辑和新建用户资料时常住地址可勾选“常住地同工作地”

--- app/controllers/user/info_controller.rb
+++ app/controllers/user/info_controller.rb
@@ -303,21 +303,40 @@ class User::InfoController < Wechat::BaseController
 
   def update_user_info_review
     @user_info_review = UserInfoReview.find_by(id: params[:id])
-    format_data = {
-      identity: params[:identity],
-      work_province: params[:work_address][:province],
-      work_city: params[:work_address][:city],
-      work_street: params[:work_address][:street],
-      resident_province: params[:resident_address][:province],
-      resident_city: params[:resident_address][:city],
-      resident_street: params[:resident_address][:street]
-    }
-    if @user_info_review.blank?
-      review = UserInfoReview.new(format_data)
-      review.user_id = current_user.user.id
-      review.save!
+    if params[:sameaddress] == "on"
+        format_data = {
+          identity: params[:identity],
+          work_province: params[:work_address][:province],
+          work_city: params[:work_address][:city],
+          work_street: params[:work_address][:street],
+          resident_province: params[:work_address][:province],
+          resident_city: params[:work_address][:city],
+          resident_street: params[:work_address][:street]
+        }
+        if @user_info_review.blank?
+          review = UserInfoReview.new(format_data)
+          review.user_id = current_user.user.id
+          review.save!
+        else
+          @user_info_review.update(format_data)
+        end
     else
-      @user_info_review.update(format_data)
+        format_data = {
+          identity: params[:identity],
+          work_province: params[:work_address][:province],
+          work_city: params[:work_address][:city],
+          work_street: params[:work_address][:street],
+          resident_province: params[:resident_address][:province],
+          resident_city: params[:resident_address][:city],
+          resident_street: params[:resident_address][:street]
+        }
+        if @user_info_review.blank?
+          review = UserInfoReview.new(format_data)
+          review.user_id = current_user.user.id
+          review.save!
+        else
+          @user_info_review.update(format_data)
+        end
     end
 
     redirect_to user_setting_path
