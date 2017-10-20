ab64407e82668615a4557ba1909fdf6047f415a2
zhou <1561490249@qq.com>
2017-10-20 15:05:43 +0800

    健康管理师和工作站页面访问限制

--- app/controllers/doctor/home_controller.rb
+++ app/controllers/doctor/home_controller.rb
@@ -1,5 +1,8 @@
 class Doctor::HomeController < Wechat::BaseController
   def index
+  	  if current_user.user.status != "Staff"
+  	  	redirect_to :back, notice: "只有健康管理师才能访问该模板的权限!"
+  	  end
       @slides = Slide.top(1)
       @user_info_review = UserInfoReview.where(identity: 'family_doctor',
       identity: 'family_health_manager',identity: 'helath_manager').order(ranking: :desc).limit(10)
