863af65e7a8db75992c061a36991604c93c5db05
skjmy <skjmy@Outlook.com>
2017-10-27 16:50:58 +0800

    工作站 更名为 服务中心

--- app/admin/workstation.rb
+++ app/admin/workstation.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Workstation do
   menu parent: I18n.t("active_admin.menu.serve_manage")
   permit_params :name,:city
   form(:html => { :multipart => true }) do |f|
-    f.inputs "工作站" do
+    f.inputs "服务中心" do
       f.input :name
       f.input :city
     end
