934341ccbdaef75baf5fd0f315578684e0142700
DESKTOP-487PIDN\Skjmy <skjmy@outlook.com>
2017-11-13 15:57:00 +0800

    查看患者基本数据

--- app/controllers/doctor/patient_controller.rb
+++ app/controllers/doctor/patient_controller.rb
@@ -1,7 +1,8 @@
 class Doctor::PatientController < Wechat::BaseController
   def info
-    if !params[:format].nil?
-      @idcard = User.find(params[:format])
+    if !params[:id].nil?
+      @idcard = User.find(params[:id])
+      @id = params[:id]
     else
       @idcard = User.find(current_user.user_id)
     end
