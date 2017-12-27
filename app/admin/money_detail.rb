520e5b26a3d7a924960f2338c1df4e9de5bd05f7
diyu123 <694310292@qq.com>
2017-12-27 13:48:27 +0800

    创建个人收益记录

--- app/admin/money_detail.rb
+++ app/admin/money_detail.rb
@@ -1,10 +1,11 @@
 ActiveAdmin.register MoneyDetail do
     menu parent: I18n.t("active_admin.menu.parallel_shop_manage")
-    permit_params :user_id, :reason, :shop_order_id, :record_id, :money
+    permit_params :user_id, :reason, :shop_order_id, :record_id, :money, :plan
     form(:html => { :multipart => true }) do |f|
       f.inputs "月结" do
         f.input :user
         f.input :shop_order
+        f.input :plan
         f.input :record_id
         f.input :reason
         f.input :money
@@ -17,6 +18,7 @@ ActiveAdmin.register MoneyDetail do
         id_column
         column :user
         column :shop_order
+        column :plan
         column :record_id
         column :reason
         column :money
