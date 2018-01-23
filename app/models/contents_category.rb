01980102dc12ec788f4282359c1391f1c0d81840
zhouyi4111 <18686471019@163.com>
2018-01-23 23:14:40 +0800

    添加产品目录model

---
+++ app/models/contents_category.rb
@@ -0,0 +1,5 @@
+class ContentsCategory < ApplicationRecord
+  has_many :products
+  has_many :downs, class_name: "ContentsCategory", foreign_key: "up_id"
+  belongs_to :up, class_name: "ContentsCategory"
+end
