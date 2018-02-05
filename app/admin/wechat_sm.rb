64fdc3315cc8b7ec8c3cf44b32faaf66f078a994
zhouyi4111 <18686471019@163.com>
2018-02-05 19:44:54 +0800

    添加群发功能  （群发微信模板消息）

--- app/admin/wechat_sm.rb
+++ app/admin/wechat_sm.rb
@@ -36,36 +36,38 @@ ActiveAdmin.register WechatSm do
 
   controller do
     def send_test
-      sms = WechatSm.find(params[:id])
-      data = {
-          first: {
-              value: sms.first,
-              color: "#173177"
-          },
-          keyword1: {
-              value: sms.keyword1,
-              color: "#173177"
-          },
-          keyword2: {
-              value: sms.keyword2,
-              color: "#173177"
-          },
-          keyword3: {
-              value: sms.keyword3,
-              color: "#173177"
-          },
-          remark: {
-              value: sms.remark,
-              color: "#173177"
-          }
-      }
+      WechatUser.find_each(batch_size: 100) do |user|
+        sms = WechatSm.find(params[:id])
+        data = {
+            first: {
+                value: sms.first,
+                color: "#173177"
+            },
+            keyword1: {
+                value: sms.keyword1,
+                color: "#173177"
+            },
+            keyword2: {
+                value: sms.keyword2,
+                color: "#173177"
+            },
+            keyword3: {
+                value: sms.keyword3,
+                color: "#173177"
+            },
+            remark: {
+                value: sms.remark,
+                color: "#173177"
+            }
+        }
+        Settings.weixin.template_id = sms.template
+        # Settings.weixin.template_id = "tYUWJ1saT3ZEH4YtV8JnUhqb94_okcU16b1gg-0RvaY"
+        url = Settings.weixin.host + "/mall"
+        open_id = User.find(user.id).wechat_user.open_id
 
-      Settings.weixin.template_id = sms.template
-      # Settings.weixin.template_id = "tYUWJ1saT3ZEH4YtV8JnUhqb94_okcU16b1gg-0RvaY"
-      url = Settings.weixin.host + "/mall"
-      open_id = User.find(1).wechat_user.open_id
-
-      $wechat_client.send_template_msg(open_id, Settings.weixin.template_id, url, "#FD878E", data)
+        $wechat_client.send_template_msg(open_id, Settings.weixin.template_id, url, "#FD878E", data)
+        logger.info("========================群发变更通知=====================")
+      end
       redirect_to admin_wechat_sms_path
     end
   end
