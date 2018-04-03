afad7ee5d51a0d1305026aa74e01c5a3ba9199a2
zwfang <zongwu77@126.com>
2018-04-03 10:00:27 +0800

    添加 “分类详情” class_detail页面

--- app/controllers/mall/home_controller.rb
+++ app/controllers/mall/home_controller.rb
@@ -21,4 +21,22 @@ class Mall::HomeController < Mall::BaseController
   	@products = @q.result(distinct: true).select("image,name,general,now_product_price,original_product_price,is_test,priority,display,id").where(display: true).order("priority DESC, id DESC")
     @programs = Program.where(is_show: true)
   end
+
+  def class_detail 
+    @slides = Slide.top(2)
+    @q = Product.ransack(params[:q])
+    @products = @q.result(distinct: true).select("image,name,general,now_product_price,original_product_price,is_test,priority,display,id").where(display: true).order("priority DESC, id DESC")
+    if !params[:id].nil?
+      @products = @q.result(distinct: true).where(display: true,is_show: true,height: "一盏明灯")
+      @slides = Slide.top(9)
+    end
+    @id = params[:id]
+
+    # 判断用户不是测试人员时， 筛选非测试产品
+    if current_user.user && current_user.user.is_test == false
+      @products = @products.where(is_test: false, contents_category_id: ContentsCategory.find(1).downs)
+    end
+    @first_categorys = ContentsCategory.where(up_id: nil, is_display: true)
+  end
+
 end
