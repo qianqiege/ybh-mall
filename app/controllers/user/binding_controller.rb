69d1e4ee396c07b6570d6b6cd0eba2c8bbf88ddc
zhouyi4111 <18686471019@163.com>
2018-04-17 16:37:05 +0800

    注册成功跳转到签订承诺书页面

--- app/controllers/user/binding_controller.rb
+++ app/controllers/user/binding_controller.rb
@@ -28,11 +28,11 @@ class User::BindingController < Wechat::BaseController
       # 生成系统身份码
       size = 18
       charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z}
-      identity_card = (0...size).map{ charset.to_a[rand(charset.size)] }.join
+      identity_card = (0...size).map {charset.to_a[rand(charset.size)]}.join
 
       @invitation_user = User.find_by(invitation_card: params[:invitation_id])
 
-      @user = User.new(name: params[:name],telphone: params[:mobile],password: params[:password],identity_card: identity_card,invitation_card: @invitation_card,status: "User",ybz_number: 0)
+      @user = User.new(name: params[:name], telphone: params[:mobile], password: params[:password], identity_card: identity_card, invitation_card: @invitation_card, status: "User", ybz_number: 0)
 
       # # 如果原来已经有推荐人了，注册直接绑定推荐人
       # if current_user.recommender.present?
@@ -40,32 +40,30 @@ class User::BindingController < Wechat::BaseController
       # end
 
 
-
-# <ActionController::Parameters {"utf8"=>"✓", "authenticity_token"=>"taVYJ/8M+MipUwooFvYSrfmrmnXepGkWXGxLFXjwB9GOOu12QAYXLD1iBSgvQzLQgBnny3lKrpRRcOk0+s774g==",
-#   "invitation_id"=>"", "name"=>"zhoufabin", "password"=>"123456", "identity"=>"family_health_manager", "work_address"=>{"province"=>"340000", "city"=>"341600",
-#     "street"=>"341622"}, "resident_address"=>{"province"=>"320000", "city"=>"321100", "street"=>"321182"}, "mobile"=>"18888653566", "code"=>"173401",
-#     "agreeCheck"=>"on", "controller"=>"user/binding", "action"=>"bind_phone"} permitted: false>
-
+      # <ActionController::Parameters {"utf8"=>"✓", "authenticity_token"=>"taVYJ/8M+MipUwooFvYSrfmrmnXepGkWXGxLFXjwB9GOOu12QAYXLD1iBSgvQzLQgBnny3lKrpRRcOk0+s774g==",
+      #   "invitation_id"=>"", "name"=>"zhoufabin", "password"=>"123456", "identity"=>"family_health_manager", "work_address"=>{"province"=>"340000", "city"=>"341600",
+      #     "street"=>"341622"}, "resident_address"=>{"province"=>"320000", "city"=>"321100", "street"=>"321182"}, "mobile"=>"18888653566", "code"=>"173401",
+      #     "agreeCheck"=>"on", "controller"=>"user/binding", "action"=>"bind_phone"} permitted: false>
 
 
       if params[:identity] != "user"
-          if params[:sameaddress] == "on"
-                @user_info_review = UserInfoReview.new(identity: params[:identity],
-                             work_province: params[:work_address][:province],
-                             work_city: params[:work_address][:city],
-                             work_street: params[:work_address][:street],
-                             resident_province: params[:work_address][:province],
-                             resident_city: params[:work_address][:city],
-                             resident_street: params[:work_address][:street]);
-            else
-                @user_info_review = UserInfoReview.new(identity: params[:identity],
-                                   work_province: params[:work_address][:province],
-                                   work_city: params[:work_address][:city],
-                                   work_street: params[:work_address][:street],
-                                   resident_province: params[:resident_address][:province],
-                                   resident_city: params[:resident_address][:city],
-                                   resident_street: params[:resident_address][:street]);
-            end
+        if params[:sameaddress] == "on"
+          @user_info_review = UserInfoReview.new(identity: params[:identity],
+                                                 work_province: params[:work_address][:province],
+                                                 work_city: params[:work_address][:city],
+                                                 work_street: params[:work_address][:street],
+                                                 resident_province: params[:work_address][:province],
+                                                 resident_city: params[:work_address][:city],
+                                                 resident_street: params[:work_address][:street]);
+        else
+          @user_info_review = UserInfoReview.new(identity: params[:identity],
+                                                 work_province: params[:work_address][:province],
+                                                 work_city: params[:work_address][:city],
+                                                 work_street: params[:work_address][:street],
+                                                 resident_province: params[:resident_address][:province],
+                                                 resident_city: params[:resident_address][:city],
+                                                 resident_street: params[:resident_address][:street]);
+        end
       end
 
       if @invitation_user.present?
@@ -73,42 +71,50 @@ class User::BindingController < Wechat::BaseController
         @user.staff_invitation_type = @invitation_user.status
       end
 
-      if(params[:is_doctor])
+      if (params[:is_doctor])
         @user.type = "Doctor"
       else
         @user.type = "Patient"
       end
 
       if @user_info_review.blank?
-         if @user.save && @wechat = WechatUser.find(current_user.id).update(user_id: @user.id)
-           flash[:notice] = '恭喜您，注册成功'
+        if @user.save && @wechat = WechatUser.find(current_user.id).update(user_id: @user.id)
+          flash[:notice] = '恭喜您，注册成功'
 
-           # 若是从影子店扫营业员二维码，  则回到影子店页面
-           if params[:waiter_id] != ""
-             redirect_to wechat_parallel_shops_shopdata_path(money: params[:money], waiter_id: params[:waiter_id])
-           else
+          # 若是从影子店扫营业员二维码，  则回到影子店页面
+          if params[:waiter_id] != ""
+            redirect_to wechat_parallel_shops_shopdata_path(money: params[:money], waiter_id: params[:waiter_id])
+          else
             redirect_to root_path
-           end  
+          end
 
 
-           @user.send_template_msg
-           @user.send_message
-           return
-         else
-           flash[:notice] = '对不起，注册失败'
-           redirect_back fallback_location: user_binding_path
-         end
+          @user.send_template_msg
+          @user.send_message
+          return
+        else
+          flash[:notice] = '对不起，注册失败'
+          redirect_back fallback_location: user_binding_path
+        end
       else
-         if @user.save && @wechat = WechatUser.find(current_user.id).update(user_id: @user.id) && @user_info_review.save
-            @user_info_review.update!(user_id: @user.id)
-            flash[:notice] = '你的信息正在审核中'
-            redirect_to root_path
-         else
-            flash[:notice] = '注册失败，请检查您的信息'
-            redirect_back fallback_location: user_binding_path
-         end
+        if @user.save && @wechat = WechatUser.find(current_user.id).update(user_id: @user.id) && @user_info_review.save
+          @user_info_review.update!(user_id: @user.id)
+          flash[:notice] = '你的信息正在审核中'
+          redirect_to root_path
+        else
+          flash[:notice] = '注册失败，请检查您的信息'
+          redirect_back fallback_location: user_binding_path
+        end
       end
-
+      if params[:entrustment] == 1
+        if @user.save && @wechat = WechatUser.find(current_user.id).update(user_id: @user.id)
+          redirect_to 'wechat/show_consumer_entrustment', notice:  '恭喜您，注册成功'
+          return
+        else
+          flash[:notice] = '对不起，注册失败'
+          redirect_back fallback_location: user_binding_path
+        end
+      end
     else
       flash[:notice] = '验证码无效或过期, 请重新发送验证码'
       redirect_back fallback_location: user_binding_path
