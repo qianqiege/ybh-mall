83f5991a0c82268e9eda4eff1cfb1ad7ed2f2f5e
zhouyi4111 <18686471019@163.com>
2018-04-25 01:53:26 +0800

    商城产品列表按优先级显示。商品价格大于1万元以万为单位

--- app/controllers/mall/products_controller.rb
+++ app/controllers/mall/products_controller.rb
@@ -12,10 +12,10 @@ class Mall::ProductsController < Mall::BaseController
     @products = Product.select("image,name,general,now_product_price,original_product_price,is_test,priority,display,id").where(contents_category_id: params[:id], display: true, is_test: false).order("priority DESC, id DESC")
     render partial: '/mall/home/product_classified'
   end
-
+  # 分类详情 查询展示二级分类产品
   def second_content
     @second_category = ContentsCategory.find(params[:id]).downs.where(is_display: true)
-    @products = Product.where(display: true, contents_category_id: params[:id])
+    @products = Product.where(display: true, contents_category_id: params[:id]).order("priority DESC, id DESC")
     render partial: '/mall/home/product_classified'
   end
 
